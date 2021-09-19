2996f2072ede89f56b0b367843112acd70fcf9e8
t-yama-3 <pxbdg597@ybb.ne.jp>
2021-09-19 17:30:45 +0900

    create interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,17 @@
+export {};
+
+type ObjectType = {
+  name: string;
+  age: number;
+};
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object: ObjectInterface = {
+  name: 'Ham-san',
+  age: 43
+};
+console.log(object);
